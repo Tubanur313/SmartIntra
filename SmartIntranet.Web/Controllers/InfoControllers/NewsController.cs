cd2d37d0f0f4df4a0f6e0f39129fbe34a1f51a49
nilkin <ilkinnazarli@gmail.com>
2022-05-13 00:20:18 +0400

    14

--- SmartIntranet.Web/Controllers/InfoControllers/NewsController.cs
+++ SmartIntranet.Web/Controllers/InfoControllers/NewsController.cs
@@ -115,6 +115,85 @@ namespace SmartIntranet.Web.Controllers.InfoControllers
                 {
                     foreach (int categoryId in model.CategoriesId)
                     {
+                        CategoryNewsAddDto confirm = new CategoryNewsAddDto
+                        {
+                            CategoryId = categoryId,
+                            NewsId = result.Id
+                        };
+                        var map = _map.Map<CategoryNews>(confirm);
+                        map.CreatedByUserId = GetSignInUserId();
+                        var confirmers = await _categoryNewsService
+                        .AddReturnEntityAsync(map);
+                    }
+                }
+                TempData["success"] = Messages.Add.Added;
+                return RedirectToAction("List");
+            }
+            else
+            {
+                TempData["error"] = Messages.Error.notComplete;
+                return RedirectToAction("List");
+            }
+        }
+
+        [HttpGet]
+        [Authorize(Policy = "news.update")]
+        public async Task<IActionResult> Update(int id)
+        {
+            var data = _map.Map<NewsUpdateDto>(await _newsService.FindByIdIncludeAllAsync(id));
+            if (data is null)
+            {
+                TempData["error"] = Messages.Error.notFound;
+                return RedirectToAction("List");
+            }
+            ViewBag.categories = _map
+                .Map<ICollection<CategoryListDto>>(await _categoryService.GetAllAsync(x => !x.IsDeleted));
+            return View(data);
+        }
+        [HttpPost]
+        [Authorize(Policy = "news.update")]
+        public async Task<IActionResult> Update(NewsAddDto model, List<IFormFile> uploads)
+        {
+            if (ModelState.IsValid)
+            {
+                var data = await _newsService.FindByIdAsync(model.Id);
+                var update = _map.Map<News>(model);
+                update.UpdateByUserId = GetSignInUserId();
+                update.CreatedByUserId = data.CreatedByUserId;
+                update.DeleteByUserId = data.DeleteByUserId;
+                update.CreatedDate = data.CreatedDate;
+                update.UpdateDate = DateTime.Now;
+                update.DeleteDate = data.DeleteDate;
+
+                var result = await _newsService.UpdateReturnEntityAsync(update);
+                if (result.Id == 0)
+                {
+                    TempData["error"] = Messages.Add.notAdded;
+                    return RedirectToAction("List");
+                }
+                if (uploads.Count > 0)
+                {
+                    foreach (var upload in uploads)
+                    {
+                        if (!MimeTypeCheckExtension.İsImage(uploads))
+                        {
+                            TempData["error"] = Messages.Error.wrongFormat;
+                        }
+                        else
+                        {
+                            NewsFileAddDto file = new NewsFileAddDto()
+                            {
+                                Name = _upload.UploadResizedImg(upload, "wwwroot/news/"),
+                                NewsId = result.Id
+                            };
+                            await _newsfileService.AddAsync(_map.Map<NewsFile>(file));
+                        }
+                    }
+                }
+                if (model.CategoriesId.Count > 0)
+                {
+                    foreach (int categoryId in model.CategoriesId)
+                    {
                         if (!await _categoryNewsService
                             .AnyAsync(x => x.NewsId == result.Id && x.CategoryId == categoryId))
                         {
@@ -130,29 +209,11 @@ namespace SmartIntranet.Web.Controllers.InfoControllers
                         }
                     }
                 }
-                TempData["success"] = Messages.Add.Added;
+                TempData["success"] = Messages.Update.Updated;
                 return RedirectToAction("List");
             }
-            else
-            {
-                TempData["error"] = Messages.Error.notComplete;
-                return RedirectToAction("List");
-            }
-        }
-        
-        [HttpGet]
-        [Authorize(Policy = "news.update")]
-        public async Task<IActionResult> Update(int id)
-        {
-            var data = _map.Map<NewsUpdateDto>(await _newsService.FindByIdIncludeAllAsync(id));
-            if (data is null)
-            {
-                TempData["error"] = Messages.Error.notFound;
-                return RedirectToAction("List");
-            }
-            ViewBag.categories = _map
-                .Map<ICollection<CategoryListDto>>(await _categoryService.GetAllAsync(x => !x.IsDeleted));
-            return View(data);
+            TempData["error"] = Messages.Error.notComplete;
+            return RedirectToAction("List");
         }
     }
 }
