0ae6044acd9508df5238aef18db834b515ed7e73
Mahir <Mahir@DESKTOP-A5K0L10>
2022-05-18 13:59:09 +0400

    1

--- SmartIntranet.DTO/DTOs/NewsDto/NewsListDto.cs
+++ SmartIntranet.DTO/DTOs/NewsDto/NewsListDto.cs
@@ -1,9 +1,7 @@
-﻿using SmartIntranet.Entities.Concrete;
-using SmartIntranet.Entities.Concrete.Intranet;
+﻿using SmartIntranet.Entities.Concrete.Intranet;
 using SmartIntranet.Entities.Concrete.Membership;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace SmartIntranet.DTO.DTOs.NewsDto
 {
