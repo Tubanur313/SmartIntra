3022540f11299813ca4a1745d43423eb7de13501
XadicaAllahverdi <XadicaAllahverdi@bitbucket.org>
2022-06-26 23:13:43 +0400

    corretion context

--- SmartIntranet.DataAccess/Concrete/EntityFrameworkCore/Context/IntranetContext.cs
+++ SmartIntranet.DataAccess/Concrete/EntityFrameworkCore/Context/IntranetContext.cs
@@ -16,7 +16,7 @@ namespace SmartIntranet.DataAccess.Concrete.EntityFrameworkCore.Context
             //Xadica's local connection string
              optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=RealIntranetSmart2;Trusted_Connection=False;MultipleActiveResultSets=true"); 
 
-            optionsBuilder.UseSqlServer(@"Server=178.63.85.231;Initial Catalog=DemoIntranet3;User Id=mahir;Password=p8Mfs4&6;MultipleActiveResultSets=true");
+            //optionsBuilder.UseSqlServer(@"Server=178.63.85.231;Initial Catalog=DemoIntranet3;User Id=mahir;Password=p8Mfs4&6;MultipleActiveResultSets=true");
 
             //Ilkin's local connection string
             //optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=IntranetSmart;Trusted_Connection=False;MultipleActiveResultSets=true");
