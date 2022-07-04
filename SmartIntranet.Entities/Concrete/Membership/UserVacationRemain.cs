602e08069aeaecacd3179e2d8200f55019987f32
BakhtiyarShamilzada <bakhtiyarkhsh@code.edu.az>
2022-07-04 23:47:31 +0400

    correction for date

--- SmartIntranet.Entities/Concrete/Membership/UserVacationRemain.cs
+++ SmartIntranet.Entities/Concrete/Membership/UserVacationRemain.cs
@@ -2,13 +2,15 @@
 using SmartIntranet.Core.Entities.Concrete;
 using System;
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace SmartIntranet.Entities.Concrete
 {
     public class UserVacationRemain : BaseEntity
     {
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime FromDate { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime ToDate { get; set; }
         public decimal VacationCount { get; set; }
         public decimal UsedCount { get; set; }
