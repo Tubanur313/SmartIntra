27e1b484acf4432aaecd11ac8a34ce8ed117d8d0
Mahir <Mahir@DESKTOP-A5K0L10>
2022-07-14 14:47:40 +0400

    1

--- SmartIntranet.DataAccess/Concrete/EntityFrameworkCore/Context/IntranetContext.cs
+++ SmartIntranet.DataAccess/Concrete/EntityFrameworkCore/Context/IntranetContext.cs
@@ -17,10 +17,10 @@ namespace SmartIntranet.DataAccess.Concrete.EntityFrameworkCore.Context
             //Xadica's local connection string
             //optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=RealIntranetSmart2;Trusted_Connection=False;MultipleActiveResultSets=true"); 
 
-            //optionsBuilder.UseSqlServer(@"Server=178.63.85.231;Initial Catalog=DemoIntranet4;User Id=mahir;Password=p8Mfs4&6;MultipleActiveResultSets=true");
+            optionsBuilder.UseSqlServer(@"Server=178.63.85.231;Initial Catalog=DemoIntranet4;User Id=mahir;Password=p8Mfs4&6;MultipleActiveResultSets=true");
 
             //Ilkin's local connection string
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=IntranetSmartTest;Trusted_Connection=False;MultipleActiveResultSets=true");
+            //optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=IntranetSmartTest;Trusted_Connection=False;MultipleActiveResultSets=true");
 
             base.OnConfiguring(optionsBuilder);
         }
